e9dfa8e8824a980e0ca2cc533a0f2c9a07c89e68
GitHub Action <action@github.com>
2022-03-31 08:32:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/pythonicart/blockagent/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/pythonicart/blockagent/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pythonicart.blockagent.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pythonicart.blockagent.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
